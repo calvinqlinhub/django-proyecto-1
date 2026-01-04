6c92bf5d675f74ff9f0166215d6e68214db6f53d
calvinqlinhub <edu.poveda81@gmail.com>
2026-01-04 13:42:45 +0100

    Add home route

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("OK - Django funcionando")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", home),              # 👈 Ruta /
+    path("admin/", admin.site.urls),
 ]
